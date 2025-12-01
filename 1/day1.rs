2a2402d1eebe0212c12e712f9383b6a7d0d94739
jzacharia <9064844+jzacharia@users.noreply.github.com>
2025-12-01 12:09:23 -0800

    day 1 solved

---
+++ 1/day1.rs
@@ -0,0 +1,34 @@
+use std::fs;
+
+
+fn main() {
+
+    let contents = fs::read_to_string("input.txt").expect("should be able to read");
+
+    let mut counter = 0;
+    let mut current = 50;
+
+
+    for line in contents.lines() {
+        let (dir, num_str) = line.split_at(1);
+        let mut steps: i32 = num_str.parse().expect("invalid num");
+
+        steps = steps % 100;
+        let delta = match dir {
+            "R" => steps,
+            "L" => -steps,
+            _ => panic!("panik!
+            ")
+        };
+
+
+        current = (current + delta).rem_euclid(100);
+        
+        if current == 0 {
+            counter += 1
+        }
+
+    }
+    println!("{counter}");
+}
+
