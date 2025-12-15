be005bc71782bf37cabea062a5d736fb9af8bb02
Jake <9064844+jakezacharia@users.noreply.github.com>
2025-12-15 14:30:06 -0800

    Delete target directory

--- target/debug/deps/day1-b85a9665236d4adb.d
+++
@@ -1,5 +0,0 @@
-/Users/jacobzacharia/Documents/advent2025/target/debug/deps/libday1-b85a9665236d4adb.rmeta: src/bin/day1.rs
-
-/Users/jacobzacharia/Documents/advent2025/target/debug/deps/day1-b85a9665236d4adb.d: src/bin/day1.rs
-
-src/bin/day1.rs:
--- target/debug/deps/day2-fd43f0d620a742b2.d
+++
@@ -1,5 +0,0 @@
-/Users/jacobzacharia/Documents/advent2025/target/debug/deps/day2-fd43f0d620a742b2: src/bin/day2.rs
-
-/Users/jacobzacharia/Documents/advent2025/target/debug/deps/day2-fd43f0d620a742b2.d: src/bin/day2.rs
-
-src/bin/day2.rs:
